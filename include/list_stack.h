7a5ab30287d48196893889d43016a32f6adfa312
Yevhenii Chernykh <y.chernykh@samsung.com>
2014-07-03 17:22:15 +0300

    macro fix

--- include/list_stack.h
+++ include/list_stack.h
@@ -1,6 +1,6 @@
 #ifndef LIST_STACK_H_INCLUDED__
-#define #ifndef LIST_STACK_H_INCLUDED__
+#define LIST_STACK_H_INCLUDED__
 
 typedef struct list_stack_type * list_stack_t;
 
-#endif /* #ifndef LIST_STACK_H_INCLUDED__ */
+#endif /* LIST_STACK_H_INCLUDED__ */
