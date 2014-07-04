a0635b07fbcc1e79500eb88480b2eafd4d39cfbb
Yevhenii Chernykh <y.chernykh@samsung.com>
2014-07-04 18:00:56 +0300

    Basic stack template

--- include/list_stack.h
+++ include/list_stack.h
@@ -3,4 +3,14 @@
 
 typedef struct list_stack_type * list_stack_t;
 
+extern list_stack_t list_stack_new(void);
+
+extern int list_stack_empty(list_stack_t stack);
+
+extern void list_stack_push(list_stack_t stack, void * data);
+
+extern void * list_stack_pop(list_stack_t stack);
+
+extern void list_stack_free(list_stack_t * stack);
+
 #endif /* LIST_STACK_H_INCLUDED__ */
--- src/list_stack.c
+++ src/list_stack.c
@@ -1,1 +1,35 @@
 #include "list_stack.h"
+
+#include <assert.h>
+#include <stddef.h>
+
+struct list_stack_type
+{
+};
+
+list_stack_t list_stack_new(void)
+{
+    return NULL;
+}
+
+int list_stack_empty(list_stack_t stack)
+{
+    assert(stack);
+    return 0;
+}
+
+void list_stack_push(list_stack_t stack, void * data)
+{
+    assert(stack);
+}
+
+void * list_stack_pop(list_stack_t stack)
+{
+    assert(stack);
+    return NULL;
+}
+
+void list_stack_free(list_stack_t * stack)
+{
+    assert(stack);
+}
