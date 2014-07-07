07dd0addbe3c687a04e69f3c38d1f1e4d759d847
Yevhenii Chernykh <y.chernykh@samsung.com>
2014-07-07 19:44:13 +0300

    Simple atom interface

---
+++ include/atom.h
@@ -0,0 +1,12 @@
+/*
+ * Atom interface.
+ */
+
+#ifndef ATOM_H_INCLUDED__
+#define ATOM_H_INCLUDED__
+
+extern const char *atom_string(const char *str);
+
+extern int atom_length(const char *str);
+
+#endif /* ATOM_H_INCLUDED__ */
---
+++ src/atom.c
@@ -0,0 +1,18 @@
+#include "atom.h"
+
+#include <assert.h>
+#include <stddef.h>
+
+const char *atom_string(const char *str)
+{
+    assert(str);
+
+    return NULL;
+}
+
+int atom_length(const char *str)
+{
+    assert(str);
+
+    return 0;
+}
